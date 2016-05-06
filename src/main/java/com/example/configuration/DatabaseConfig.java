57319a89211c43ab92fbb58a9ec508f924673fab
Denys Stoianov <denys.stoianov@auto1.com>
2016-05-06 19:08:41 +0200

    add some examples

---
+++ src/main/java/com/example/configuration/DatabaseConfig.java
@@ -0,0 +1,33 @@
+package com.example.configuration;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+/**
+ * Created by denys.stoianov on 2016-05-06.
+ */
+@Configuration
+public class DatabaseConfig {
+    @Resource
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(env.getProperty("jdbc.driver"));
+        ds.setUrl(env.getProperty("jdbc.url"));
+        ds.setUsername(env.getProperty("jdbc.username"));
+        ds.setPassword(env.getProperty("jdbc.password"));
+        return ds;
+    }
+
+//    @Bean
+//    public JdbcTemplate jdbcTemplate() {
+//        return new JdbcTemplate(dataSource());
+//    }
+}
